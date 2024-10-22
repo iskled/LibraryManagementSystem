f893b244459193bb16d715dfe54a567c0ffe2a76
Iskled <iskled.adebayo@gmail.com>
2024-10-22 23:44:42 +0100

    multi-thread functions were developed and tested from the main.cpp

--- main.cpp
+++ main.cpp
@@ -11,6 +11,9 @@ Author: Iskled Adebayo
 #include <format>
 #include <vector>
 #include <fstream>
+#include <map>
+#include <sstream>#
+#include <thread>
 
 
 using namespace std;
@@ -22,14 +25,16 @@ int main() {
 		cout << "1. File processing\n";
 		cout << "2. Book catalogu\n";
 		cout << "3. Populate catalogue vector with file\n";
-		cout << "4. Quit\n";
+		cout << "4. Map processing and storage in the text file\n";
+		cout << "5. Demostrate multitasking\n";
+		cout << "6. Quit\n";
 		cout << "**********************************************************\n\n";
 
 		int menu;
 		do {
-			cout << "Enter 1 - 4 for menu items : ";
+			cout << "Enter 1 - 6 for menu items : ";
 			cin >> menu;
-		} while (menu < 1 || menu >4);
+		} while (menu < 1 || menu >6);
 
 		switch (menu) {
 
@@ -93,8 +98,65 @@ int main() {
 			catalogue.push_back(book);
 			break;
 		}
-
 		case 4:
+		{
+			// Map processing and storage in the text file
+
+			map<string, Officer> staff;
+			char choice;
+			do{
+
+				cout << "Enter name of officer to process\n";
+				string fullName, firstName;
+				cin.ignore();
+				getline(cin, fullName);
+				istringstream iss(fullName);
+				iss >> firstName;
+
+				Officer officer;
+				officer.fullName = fullName;
+				mPopulate(officer);
+
+				staff.insert({ firstName, officer });
+
+				cout << "File successfully created on the map\n";
+
+				cout << "Press Y (to continue) or N (to Quit)\n";
+				cin >> choice;
+			} while (choice != 'N');
+
+
+				cout << "File now being written to the text file\n";
+
+				ofstream outFile;
+				outFile.open("staff.txt");
+
+				if (!outFile.is_open()) {
+					cout << "*** File opened unsuccessfully ***\n";
+				}
+				else
+				{
+					writeFile(staff, outFile);
+					outFile.close();
+
+					cout << "*** Object written to file successfully ***\n";
+				}
+				break;
+		}
+
+		case 5:
+		{
+			//Demonstrating real-time multi tasking
+			cout << "Demonstrating real-time multi tasking\n";
+
+			thread  osThread1(function1);
+			thread osThread2(function2);
+
+			system("pause>0");
+
+			break;
+		}
+		case 6:
 		{
 			return 0;
 		}
